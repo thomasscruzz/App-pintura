05cf397e0ef3c2366117c43eaf2ea84207886a1d
thomasscruzz <thomascruz699@gmail.com>
2023-05-12 20:01:57 -0300

    5% de la app funcionando

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const path = require("path")
 const app = express();
 
 
+app.use(express.static("public"));
 
 app.listen(3000, () => {
     console.log("Servidor levantandose");
 });
 
-app.get("/home",(req,res)=>{
+app.get("/",(req,res)=>{
     res.send("Hola");
 })
+app.get("/home",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+})
+app.get('/404',(req,resp)=>{
+    resp.send('Error página no encontrada')
+ })
+ 
 // http://localhost:3000/
---
+++ controllers/productos.js
@@ -0,0 +1,6 @@
+const controlador = {
+    index:(req,res)=>{
+        res.send("Aca estan todos los productos")
+    }
+}
+module.exports = controlador;
